731685ce167e5944d6f4926bfd2f7cb60ce1fe43
Raviansyah Noorman <90436146+raaviian@users.noreply.github.com>
2025-05-04 09:45:53 +0800

    Delete: public/firebaseConfig.js

--- public/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-// public/firebaseConfig.js
-export const firebaseConfig = {
-    apiKey:           "AIzaSyAkm2Ot4xqrFqTka9LwwTzyZQhsDxZX6Z8",
-    authDomain:       "gdgapp-e4e14.firebaseapp.com",
-    projectId:        "gdgapp-e4e14",
-    storageBucket:    "gdgapp-e4e14.appspot.com",
-    messagingSenderId: "1084825943427",
-    appId:            "1:1084825943427:web:1a8563798d6419b84c4ab6"
-  };  
